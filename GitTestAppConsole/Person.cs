2ba13140a4fc3f42dbd806af6c864c7e55b5e704
Izay <itaytai@hotmail.com>
2015-12-14 18:38:54 +0200

    Some new Feature shit

--- GitTestAppConsole/Person.cs
+++ GitTestAppConsole/Person.cs
@@ -11,7 +11,8 @@ namespace GitTestAppConsole
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
-
         public string Email { get; set; }
+
+        public string NewFeatureProp { get; set; }
     }
 }
